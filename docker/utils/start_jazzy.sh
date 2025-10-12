20b9e453d9497e5c0c93ed30fd8cdf917aeb96e1
Ilia O. <Ilia O.>
2025-10-11 20:32:26 -0700

    Add start_jazzy bash

---
+++ docker/utils/start_jazzy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+xhost local:root
+XAUTH=/tmp/.docker.xauth
+
+sudo docker run -it \
+  --name=makerspet \
+  --rm \
+  -p 8888:8888/udp \
+  --env="DISPLAY=$DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --env="XAUTHORITY=$XAUTH" \
+  --volume="$XAUTH:$XAUTH" \
+  --net=host \
+  --privileged \
+  --add-host="host.docker.internal:host-gateway" \
+  kaiaai/kaiaai:jazzy
+
+  echo "Done"
