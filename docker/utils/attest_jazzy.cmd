222707e0b69b9c15327597631a88eab279b45c18
Ilia O. <iliao@kaia.ai>
2025-11-21 14:41:51 +0800

    Fix workdir

--- docker/utils/attest_jazzy.cmd
+++ docker/utils/attest_jazzy.cmd
@@ -3,6 +3,5 @@ docker image rm kaiaai/kaiaai:jazzy
 docker system prune -f -a
 docker buildx create --name kaiaai --driver=docker-container
 cd .\kaiaai
-docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:jazzy --push .
+docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:jazzy --push --load .
 docker rm buildx_buildkit_kaiaai0 --force
-docker pull kaiaai/kaiaai:jazzy
--- docker/utils/attest_jazzy_dev_cached.cmd
+++ docker/utils/attest_jazzy_dev_cached.cmd
@@ -1,4 +1,4 @@
 cd .\kaiaai
-docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:jazzy-dev --push .
+docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:jazzy-dev --push --load .
 docker rm buildx_buildkit_kaiaai0 --force
 docker pull kaiaai/kaiaai:jazzy-dev
