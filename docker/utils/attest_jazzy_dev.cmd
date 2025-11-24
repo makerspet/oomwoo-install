031ab32ea29f878794c8f9273a2e2d285649917d
Ilia O. <iliao@kaia.ai>
2025-11-23 23:17:28 -0800

    Cleanup

--- docker/utils/attest_jazzy_dev.cmd
+++ docker/utils/attest_jazzy_dev.cmd
@@ -5,4 +5,3 @@ docker buildx create --name kaiaai --driver=docker-container
 cd .\kaiaai
 docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:jazzy-dev --push --load .
 docker rm buildx_buildkit_kaiaai0 --force
-# docker pull kaiaai/kaiaai:jazzy-dev
