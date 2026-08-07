a353b57b5f2f0945165ae8034332b20e5f950ffa
Ilia O. <iliao@kaia.ai>
2026-08-07 00:05:01 -0700

    Build cached

--- docker/utils/attest_jazzy_dev_cached.cmd
+++ docker/utils/attest_jazzy_dev_cached.cmd
@@ -1,3 +1,6 @@
+docker login
+docker image rm makerspet/oomwoo:jazzy-dev
+docker buildx create --name kaiaai --driver=docker-container
 cd .\kaiaai
-docker buildx build --builder=kaiaai --no-cache --provenance=true -t makerspet/oomwoo:jazzy-dev --push --load .
+docker buildx build --builder=kaiaai --provenance=true -t makerspet/oomwoo:jazzy-dev --push --load .
 docker rm buildx_buildkit_kaiaai0 --force
